6f69caf40717e086d57d2f81218a2fb2025accd4
Cesar Acosta <cesaracosta303@gmail.com>
2020-06-10 18:11:28 +0000

    VPC added

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,14 @@
 module "vpc" {
     source          = "app.terraform.io/CesarAcosta-Training/vpc/aws"
     version         = "2.39.0"
+}
 
-    name = "Cesar VPC"
+resource aws_vpc "gaurav_vpc" {
+    cidr_block              = var.address_space
+    enable_dns_hostnames    = true
+
+    tags = {
+        name = "gaurav-own-vpc"
+    }
+
 }
