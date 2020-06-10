03fc2e23f41f28d13b1436dd74660e4e670a4acb
Cesar Acosta <cesaracosta303@gmail.com>
2020-06-10 18:16:24 +0000

    VPC file modified

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
 }
 
 resource aws_vpc "gaurav_vpc" {
-    cidr_block              = var.address_space
+    cidr_block              = "10.0.0.0/16"
     enable_dns_hostnames    = true
 
     tags = {
